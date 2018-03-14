60bcced3a59d8101135efaebdfdae9c73ff3d9d8
ravemula <raky452@outlook.com>
2018-03-14 22:48:13 +0530

    http://localhost:56925/api/Groups/raky452@outlook.com for getting groups of the user

---
+++ DeamonApp/Constants/AppConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeamonApp.Constants
+{
+    public static class AppConstants
+    {
+        public const string ClientId = "App Id [update]";
+        public const string ClientSecret = "App password [update]";
+        public const string GraphV2Uri = "https://login.microsoftonline.com/{0}/v2.0";
+        public const string TenentName = "AD Tenant name [update] ";
+        public const string RedirectUrl = "App Redirect URL [update]";
+        public const string ApplicationDefaultScope = "https://graph.microsoft.com/.default";
+        public const string Bearer = "Bearer";
+        public const string AuthorizatioHeader = "Authorization";
+        public const string GraphApiV1Url = "https://graph.microsoft.com/v1.0/users/{0}/memberOf";
+        public const string ExtensionAttachedForAd = "#EXT#@";
+    }
+}
--- DeamonApp/Controllers/ValuesController.cs
+++ DeamonApp/Controllers/ValuesController.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
+using DeamonApp.Constants;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json.Linq;
+
 
 namespace DeamonApp.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class GroupsController : Controller
     {
         // GET api/values
         [HttpGet]
@@ -18,9 +25,39 @@ namespace DeamonApp.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public JsonResult Get(string id)
         {
-            return "value";
+            string emailAddress = id;
+            string result;
+            try
+            {
+                ConfidentialClientApplication cl = new ConfidentialClientApplication(
+                AppConstants.ClientId,
+                String.Format(AppConstants.GraphV2Uri, AppConstants.TenentName),
+                AppConstants.RedirectUrl,
+                new Microsoft.Identity.Client.ClientCredential(AppConstants.ClientSecret),
+                new TokenCache(),
+                new TokenCache());
+                Microsoft.Identity.Client.AuthenticationResult authResult = cl.AcquireTokenForClientAsync(new string[] { AppConstants.ApplicationDefaultScope }).Result;
+                string bearerToken = AppConstants.Bearer + " " + authResult.AccessToken.ToString();
+                var client = new HttpClient();
+                var queryString = HttpUtility.ParseQueryString(string.Empty);
+                var userUpn = emailAddress.Replace('@', '_') + AppConstants.ExtensionAttachedForAd + AppConstants.TenentName;
+                var uri = string.Format(AppConstants.GraphApiV1Url, HttpUtility.UrlEncode(userUpn));
+                client.DefaultRequestHeaders.Add(AppConstants.AuthorizatioHeader, bearerToken);
+                var response = client.GetAsync(uri).GetAwaiter().GetResult();
+                if (response.StatusCode == HttpStatusCode.OK && response.Content != null)
+                {
+                    var responseString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    var jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(responseString)["value"];
+                    return this.Json(jsonResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.Json(new object());
+            }
+            return this.Json(new object());
         }
 
         // POST api/values
